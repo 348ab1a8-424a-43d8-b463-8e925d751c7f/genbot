4ed1330ab3bb1c5044621fc6d6de94e8872895f4
348ab1a8-424a-43d8-b463-8e925d751c7f <348ab1a8-424a-43d8-b463-8e925d751c7f@amplivum.com>
2025-04-22 11:47:55 -0400

    Initial command service.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace _07cab3d6_0d7d
+﻿using Discord;
+using Discord.Net;
+using Discord.Webhook;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+
+namespace _07cab3d6_0d7d
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static DiscordSocketClient _client;
+
+        public static async Task Main()
         {
-            Console.WriteLine("Hello, World!");
+            // Initialization
+            _client = new DiscordSocketClient();
+
+            var token = JsonConvert.DeserializeObject<IConfiguration>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        public static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
