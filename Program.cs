796a20e8095cd7fc65757dab5f50cf3d23fa585a
348ab1a8-424a-43d8-b463-8e925d751c7f <348ab1a8-424a-43d8-b463-8e925d751c7f@amplivum.com>
2025-04-22 10:25:17 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07cab3d6_0d7d
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
